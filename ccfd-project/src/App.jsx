ccca98d5f6dc2b1b8b0058263d8614e186b078c4
ayasadoq <aya.sadoq.2003.lm@gmail.com>
2025-10-15 21:17:48 +0100

    initial commit,react and tailwind setup

---
+++ ccfd-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline bg-blue-500 p-4">
+      Fraud Detection Frontend
+    </div>
+  )
+}
+// ...
+
+
+export default App
+
